949c189e347ab3dc23a045d79088f0fcc9b6f1d0
Natsukage <summer@summerlight.name>
2021-10-18 16:51:55 +0800

    添加项目文件。

---
+++ ScreenOff/HTTPServer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace ScreenOff
+{
+    public delegate void ReceivedRequestEventHandler(string payload);
+    public delegate void OnExceptionEventHandler(Exception ex);
+
+    class HttpServer
+    {
+        private Thread _serverThread;
+        private HttpListener _listener;
+        public int Port { get; private set; }
+
+        public event ReceivedRequestEventHandler ReceivedCommandRequest;
+        public event OnExceptionEventHandler OnException;
+
+        /// <summary>
+        ///     在指定端口启动监听
+        /// </summary>
+        /// <param name="port">要启动的端口</param>
+        public HttpServer(int port)
+        {
+            Initialize(port);
+        }
+
+        /// <summary>
+        ///     停止监听并释放资源
+        /// </summary>
+        public void Stop()
+        {
+            //_serverThread.Abort();
+            _listener.Stop();
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                _listener = new HttpListener();
+                _listener.Prefixes.Add("http://*:" + Port + "/");
+                _listener.Start();
+            }
+            catch (Exception ex)
+            {
+                OnException?.Invoke(ex);
+                return;
+            }
+
+            ThreadPool.QueueUserWorkItem(o => {
+                try
+                {
+                    while (_listener.IsListening)
+                        ThreadPool.QueueUserWorkItem(c =>
+                        {
+                            if (c is not HttpListenerContext context)
+                                throw new ArgumentNullException(nameof(context));
+                            try
+                            {
+                                DoAction(ref context);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            }
+                            finally
+                            {
+                                context.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                }
+                catch
+                {
+                    // ignored
+                }
+            });
+        }
+
+        private void DoAction(ref HttpListenerContext context)
+        {
+
+            var command = new StreamReader(context.Request.InputStream, Encoding.UTF8).ReadToEnd();
+
+            ReceivedCommandRequest?.Invoke(command);
+
+            var buf = Encoding.UTF8.GetBytes(command);
+            context.Response.ContentLength64 = buf.Length;
+            context.Response.OutputStream.Write(buf, 0, buf.Length);
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            context.Response.OutputStream.Flush();
+        }
+
+        private void Initialize(int port)
+        {
+            Port = port;
+            _serverThread = new Thread(Listen);
+            _serverThread.Start();
+        }
+
+    }
+}
---
+++ ScreenOff/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenOff
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")] private static extern IntPtr PostMessage(int hWnd, int msg, int wParam, int lParam);
+
+        private HttpServer _httpServer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ServerStart();
+        }
+
+        private void ServerStart(object sender = null, EventArgs e = null)
+        {
+            try
+            {
+                _httpServer = new HttpServer(30001);
+                _httpServer.ReceivedCommandRequest += DoTextCommand;
+                _httpServer.OnException += OnException;
+            }
+            catch (Exception ex)
+            {
+                OnException(ex);
+            }
+        }
+
+        private void ServerStop(object sender = null, EventArgs e = null)
+        {
+            _httpServer.Stop();
+            _httpServer.ReceivedCommandRequest -= DoTextCommand;
+            _httpServer.OnException -= OnException;
+        }
+        private void DoTextCommand(string command)
+        {
+            switch (command.ToLower(CultureInfo.CurrentCulture))
+            {
+                case "screen=off":
+                    _ = PostMessage(-1, 0x0112, 0xF170, 2);
+                    return;
+                default:
+                    return;
+            }
+        }
+        private void OnException(Exception ex)
+        {
+            string errorMessage = $"无法在{_httpServer.Port}端口启动监听\n{ex.Message}";
+            
+            MessageBox.Show(errorMessage);
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
